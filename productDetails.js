2f9fbbe6fa0f6436fc029c56f02c74a96bac5e0f
AppiahEnoch <prignutt@gmail.com>
2023-09-28 23:55:58 -0700

    www

--- productDetails.js
+++ productDetails.js
@@ -6,8 +6,10 @@
     $.ajax({
       type: "get",
       url: "getProducts.php",
-      dataType: "json",
+      dataType: "text",
       success: function (data, status) {
+
+        alert(data)
         let html = '<div class="container-fluid justify-content-center align-items-center d-flex mt-5" id="wrapper1" name="wrapper1">';
         let navbarHtml = '';
   
